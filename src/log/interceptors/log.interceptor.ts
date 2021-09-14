7de3ebd1bdb4efc7d006f63d0e4c08f5f2806463
Daniel Mwakanema <danielmwakanema95@gmail.com>
2021-09-14 10:52:17 +0200

    feat(logs): add logging module
    - added transports
    - added interceptor

---
+++ src/log/interceptors/log.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import type { Request, Response } from 'express';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { LogService } from 'src/log/log.service';
+
+@Injectable()
+export class LogInterceptor implements NestInterceptor {
+  constructor(private readonly logService: LogService) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      tap(() => {
+        const httpContext = context.switchToHttp();
+        const req = httpContext.getRequest<Request>();
+        const res = httpContext.getResponse<Response>();
+
+        const message = `Performing ${req.method} to ${req.path}`;
+
+        this.logService.info(message, { req, res });
+      }),
+    );
+  }
+}
