a71c3259391d1bcd4bfaf8e94c49ab02b97fb1dc
Daniel Mwakanema <danielmwakanema95@gmail.com>
2021-09-14 10:54:03 +0200

    chore(app): register log module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LogModule } from './log/log.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), LogModule],
   controllers: [AppController],
   providers: [AppService],
 })
