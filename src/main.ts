df5a801a10528575adaccfb80e1a8fc01a6fff6e
Daniel Mwakanema <danielmwakanema95@gmail.com>
2021-09-14 10:12:21 +0200

    refactor(app): read port and version from env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AFFIRM_API_PORT, AFFIRM_API_VERSION } from './common/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const PORT = configService.get<number>(AFFIRM_API_PORT) || 3000;
+  const VERSION = configService.get<string>(AFFIRM_API_VERSION) || 'v1';
+  const PREFIX = `/api/${VERSION}`;
+
+  app.setGlobalPrefix(PREFIX);
+  app.enableCors();
+
+  await app.listen(PORT);
 }
 bootstrap();
