a0bf4cc4d35a06a9d963a8b4365f115e2d777d9e
Daniel Mwakanema <danielmwakanema95@gmail.com>
2021-09-14 10:53:50 +0200

    refactor(app): register interceptor on ctrller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LogInterceptor } from './log/interceptors/log.interceptor';
 
+@UseInterceptors(LogInterceptor)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
