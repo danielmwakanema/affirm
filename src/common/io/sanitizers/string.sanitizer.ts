ced834481604bff29652babc1bfd3dc1dd8100d8
Daniel Mwakanema <danielmwakanema95@gmail.com>
2021-09-15 10:41:32 +0200

    feat(io): add string sanitizer

---
+++ src/common/io/sanitizers/string.sanitizer.ts
@@ -0,0 +1,17 @@
+export const StringSanitizer = {
+  trim(input: string): string {
+    return input.trim();
+  },
+  escape(input: string): string {
+    return input
+      .replace(/&/g, '&amp;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#x27;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/\//g, '&#x2F;')
+      .replace(/\\/g, '&#x5C;')
+      .replace(/`/g, '&#96;')
+      .replace(/\$/, '&#36;');
+  },
+};
