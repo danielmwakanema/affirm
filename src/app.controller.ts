8d45b884863932713b4aef5892a8b8ea61d09bf6
Daniel Mwakanema <danielmwakanema95@gmail.com>
2021-09-15 11:35:37 +0200

    refactor(app): - affirmations crud from ctrller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,80 +1,26 @@
+import { Controller, Header, Post, UseInterceptors } from '@nestjs/common';
 import {
-  Body,
-  CacheInterceptor,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-  UseInterceptors,
-} from '@nestjs/common';
-import {
-  ApiBadRequestResponse,
   ApiCreatedResponse,
   ApiInternalServerErrorResponse,
-  ApiNotFoundResponse,
-  ApiOkResponse,
 } from '@nestjs/swagger';
 import { AffirmationsService } from './affirmations/affirmations.service';
-import { CreateAffirmationDto, UpdateAffirmationDto } from './affirmations/dto';
-import { AffirmationDto } from './affirmations/dto/affirmation.dto';
-import { FindAffirmationByIdPipe } from './affirmations/pipes/find-affirmation-by-id.pipe';
-import { SanitizeAffirmationPipe } from './affirmations/pipes/sanitize-affirmation.pipe';
-import { AffirmationDocument } from './affirmations/schemas/affirmation.schema';
 import { LogInterceptor } from './log/interceptors/log.interceptor';
+import { TwilioService } from './twilio/twilio.service';
 
 @UseInterceptors(LogInterceptor)
 @Controller()
 export class AppController {
-  constructor(private readonly affirmationsService: AffirmationsService) {}
+  constructor(
+    private readonly affirmationsService: AffirmationsService,
+    private readonly twilioService: TwilioService,
+  ) {}
 
-  @ApiOkResponse({ type: AffirmationDto })
-  @ApiInternalServerErrorResponse()
-  @Get()
-  async random(): Promise<AffirmationDocument> {
-    return this.affirmationsService.random();
-  }
-
-  @ApiNotFoundResponse()
-  @ApiOkResponse({ type: AffirmationDto })
-  @ApiInternalServerErrorResponse()
-  @UseInterceptors(CacheInterceptor)
-  @Get(':id')
-  findOne(
-    @Param('id', FindAffirmationByIdPipe) affirmation: AffirmationDocument,
-  ): AffirmationDocument {
-    return affirmation;
-  }
-
-  @ApiCreatedResponse({ type: AffirmationDto })
-  @ApiBadRequestResponse()
+  @ApiCreatedResponse({ type: String })
   @ApiInternalServerErrorResponse()
   @Post()
-  async create(
-    @Body(SanitizeAffirmationPipe) data: CreateAffirmationDto,
-  ): Promise<AffirmationDocument> {
-    return this.affirmationsService.create(data);
-  }
-
-  @ApiNotFoundResponse()
-  @ApiOkResponse({ type: AffirmationDto })
-  @ApiBadRequestResponse()
-  @ApiInternalServerErrorResponse()
-  @Patch(':id')
-  async update(
-    @Param('id', FindAffirmationByIdPipe) affirmation: AffirmationDocument,
-    @Body(SanitizeAffirmationPipe) data: UpdateAffirmationDto,
-  ): Promise<AffirmationDocument> {
-    return this.affirmationsService.update(affirmation.id, data);
-  }
-
-  @ApiNotFoundResponse()
-  @ApiInternalServerErrorResponse()
-  @Delete(':id')
-  async delete(
-    @Param('id', FindAffirmationByIdPipe) affirmation: AffirmationDocument,
-  ): Promise<void> {
-    return this.affirmationsService.delete(affirmation.id);
+  @Header('Content-Type', 'text/html')
+  async random(): Promise<string> {
+    const { body } = await this.affirmationsService.random();
+    return this.twilioService.say(body);
   }
 }
