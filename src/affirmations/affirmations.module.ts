5717ed09157c5f34cd3b04f4b434c4e5a8195615
Daniel Mwakanema <danielmwakanema95@gmail.com>
2021-09-15 11:34:56 +0200

    refactor(affirmations): use separate ctrller crud

--- src/affirmations/affirmations.module.ts
+++ src/affirmations/affirmations.module.ts
@@ -1,15 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { LogModule } from '../log/log.module';
 import { AffirmationsService } from './affirmations.service';
 import { Affirmation, AffirmationSchema } from './schemas/affirmation.schema';
+import { AFFIRM_CACHE_TTL } from '../common/constants';
+import { AffirmationsController } from './affirmations.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Affirmation.name, schema: AffirmationSchema },
     ]),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (service: ConfigService) => ({
+        ttl: service.get<number>(AFFIRM_CACHE_TTL),
+      }),
+      inject: [ConfigService],
+    }),
+    LogModule,
   ],
   providers: [AffirmationsService],
   exports: [AffirmationsService],
+  controllers: [AffirmationsController],
 })
 export class AffirmationsModule {}
